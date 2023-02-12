8056ac7d64845461ba2760d6c75fe9f57178373b
g13m0 <115929947+g13m0@users.noreply.github.com>
2023-02-12 22:26:31 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum -y install https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+sudo yum --enablerepo elrepo-kernel install kernel-ml -y
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
+echo "Grub update done."
